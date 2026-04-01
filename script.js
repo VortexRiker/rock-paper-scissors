6db140c039cb3578462264486ae7f05f1a83bd4c
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-04-01 21:17:56 +0500

    Add function for selecting computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// Return a random integer value from 0 to upperBound - 1
+function getRandomInteger(upperBound)
+{
+    return Math.floor(Math.random() * upperBound);
+}
+
+function getComputerChoice()
+{
+    switch(getRandomInteger(3))
+    {
+    case 0:
+        return "Rock";
+    case 1:
+        return "Paper";
+    case 2:
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice());
